455710d8d3d1554a3833283e7a73868569846a0b
prerak1310 <preraksangwan@gmail.com>
2025-07-06 19:15:06 +0530

    role not defined error fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ exports.register = async (req, res, next) => {
       return res.status(400).json({ message: "Email already registered" });
 
     const hashed = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashed, role });
+    const user = await User.create({ name, email, password: hashed });
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "15m",
